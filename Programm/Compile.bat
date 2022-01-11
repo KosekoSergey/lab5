47c87b12ffa84bb721bd579dd16696f5dbc5935e
KosenkoSergey <winget2002@gmail.com>
2022-01-11 21:25:16 +0700

    Перенесены все классы из 4 работы на язык java

---
+++ Programm/Compile.bat
@@ -0,0 +1,8 @@
+set path=c:\Program Files\Java\jdk1.8.0_51\bin\;%path%
+javac.exe -Xstdout diagVideocard.txt Videocard.java
+javac.exe -Xstdout diagProcessor.txt Processor.java
+javac.exe -Xstdout diagRam.txt Ram.java
+javac.exe -Xstdout diagMotherboard.txt Motherboard.java
+javac.exe -Xstdout diagComputer.txt Computer.java
+javac.exe -Xstdout diagMain.txt Main.java
+pause
---
+++ Programm/Run.bat
@@ -0,0 +1,3 @@
+set path=c:\Program Files\Java\jdk1.8.0_51\bin\;%path%
+java.exe Main
+pause
